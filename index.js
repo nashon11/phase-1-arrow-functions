4bb8dc947f56473a4334913da75cb11f76aec431
NASH <nashon.mwenda@student.moringaschool.com>
2023-06-18 09:15:02 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,23 @@
+// index.js
+
+// Function expression called divide
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // Arrow function called square
+  const square = (num) => {
+    return num * num;
+  };
+  
+  // Arrow function called add
+  const add = (a, b) => {
+    return a + b;
+  };
+  
+  module.exports = {
+    divide,
+    square,
+    add
+  };
+  
